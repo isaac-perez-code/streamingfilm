61d2235216446dbeb053a96a1bf956b4f5f87d66
Isaac Perez <fredisaacperezaspajo@gmail.com>
2025-10-29 11:09:31 -0500

    feat: Enviando actualizacion Isaaaaa

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div className="min-h-screen bg-gray-100 p-8">
+        <h1>Bienvenido
+            <p className="text-2l font-bold">Esta es nuestro sitio web</p>
+        </h1>
+        </div>
+    );
+}
+
+export default Home; 
