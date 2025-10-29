f3b1c4dea6fbb4d142e6f4cf365b499dddc4bd2f
Isaac Perez <fredisaacperezaspajo@gmail.com>
2025-10-29 08:16:09 -0500

    feacture: hola bienbueno

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,45 +1,47 @@
 import express from 'express';
 import { authControllers } from '../controllers/authControllers.js';
-
 const router = express.Router();
 
 /**
- * @swagger
- * tags:
- *   name: Auth
- *   description: Endpoints para autenticación de usuarios
+ * @swagger 
+ * components:
+ *  securitySchemes:
+ *    BearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
  */
 
 /**
  * @swagger
  * /api/auth/register:
- *   post:
- *     summary: Registrar nuevo usuario
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 example: isa@gmail.com
- *               name:
- *                 type: string
- *                 example: isaac
- *               password:
- *                 type: string
- *                 example: isa1202
- *     responses:
- *       201:
- *         description: Usuario registrado exitosamente
- *       400:
- *         description: Datos enviados incorrectos
- *       500:
- *         description: Error interno del sistema
+ *  post:
+ *    summary: Registrar nuevo usuario
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: isa@gmail.com
+ *              name:
+ *                type: string
+ *                example: isaac
+ *              password:
+ *                type: string
+ *                example: isa1202
+ *    responses:
+ *      201:
+ *        description: Usuario registrado exitosamente
+ *      400:
+ *        description: Datos enviados incorrectos
+ *      500:
+ *        description: Error interno del sistema
  */
-router.post('/register', authControllers.register);
+        router.post('/register', authControllers.register)
 
 export default router;
