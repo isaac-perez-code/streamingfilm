21577e7b839bd0233c3b68853e10c3123a2b6827
celigabriel100@gmai.com <celigabriel00@gmail.com>
2025-10-31 16:26:49 -0500

    feat: Viernes actualizaccion

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,9 +1,8 @@
 function Home(){
     return(
         <div className="min-h-screen bg-gray-100 p-8">
-        <h1>Bienvenido
-            <p className="text-2l font-bold">Esta es nuestro sitio web</p>
-        </h1>
+        <h1>Bienvenido</h1>
+        <p className="text-2l font-bold">Esta es nuestro sitio web</p>
         </div>
     );
 }
